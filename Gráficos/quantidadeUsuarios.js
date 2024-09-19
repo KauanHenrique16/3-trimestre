9f6f35a4dec64f9e3d607629e70c495f387fa997
KauanHenrique16 <106541032+KauanHenrique16@users.noreply.github.com>
2024-09-19 15:27:30 -0300

    ddsdf

--- Gráficos/quantidadeUsuarios.js
+++ Gráficos/quantidadeUsuarios.js
@@ -5,7 +5,7 @@ async function quantidadeUsuarios() {
     const res = await fetch(url)
     const dados = await res.json()
     const nomeDasRedes = ['Amazonas','Bahia','Mato Grosso do Sul','Pernambuco','Roraima','Pará','Mato Grosso','Maranhão','Ceará','São Paulo','Minas Gerais','Rio Grande do sul','Acre','Paraná','Paráiba','Alagoas','Santa Catarina','Rondônia','Tocantins','Goiás','Rio de Janeiro','Espírito Santo','Rio Grande do Norte','Amapá','piauí','Distrito Federal','Sergipe']
-    const quantidadeUsuarios = ['490854','229103','116346','106634']
+    const quantidadeUsuarios = ['490854','229103','116346','106634','97320','80974','58231','57214','56353','55295','36699','36096','31699','30460','30140','25725','21541','21153','20023','19522','16964','14411','11725','11334','7198','5813','4708']
     const data = [
         {
             x: nomeDasRedes,
