df2c5143d977a2b955c395873449b5c78fa0c552
KauanHenrique16 <106541032+KauanHenrique16@users.noreply.github.com>
2024-09-26 13:38:56 -0300

    hgjgh

--- Gráficos/grafico2.js
+++ Gráficos/grafico2.js
@@ -21,7 +21,7 @@ async function quantidadeUsuarios() {
         plot_bgcolor: getCSS('--cor-de-fundo'),
         paper_bgcolor: getCSS('--cor-de-fundo'),
         title: {
-            text: 'Redes sociais com mais usuários no mundo',
+            text: 'Grafico de tribos indigenas no Brasil',
             font: {
                 color: getCSS('--cor-principal'),
                 family: getCSS('--fonte'),
